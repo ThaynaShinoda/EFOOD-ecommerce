815ebe1e493651e9dde898dd7c6359cc0d7bae30
Thayná Rie Shinoda <thayrieshinoda@gmail.com>
2025-04-09 13:25:37 -0300

    Configuração inicial

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { GlobalStyle } from './styles/styles';
+
+function App() {
+  return (
+    <>
+      <GlobalStyle />
+      <div className="wrapper">
+        <h1>EFOOD</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/styles/styles.ts
@@ -0,0 +1,28 @@
+import { createGlobalStyle } from 'styled-components';
+
+export const colors = {
+  soft_red: '#E66767',
+  peach_light: '#FFEBD9',
+  soft_cream: '#FFF8F2',
+  white: '#FFFFFF',
+};
+
+export const GlobalStyle = createGlobalStyle`
+  *{
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    font-family: "Roboto", sans-serif;
+  }
+
+  body {
+    background-color: ${colors.soft_cream};
+  }
+
+  .wrapper {
+    max-width: 64rem;
+    width: 100%;
+    margin: 0 auto;
+  }
+`;
+
